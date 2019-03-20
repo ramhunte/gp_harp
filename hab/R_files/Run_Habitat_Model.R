57fbdcb97afc40c42de36539c37027ad95d73bf3
Caleb Fogel - NOAA Affiliate <caleb.fogel@noaa.gov>
2019-03-20 18:42:39 +0000

    Merged in feature-habmodel_compare (pull request #23)
    Feature habmodel compare
    
    * Add new folder to house longform habmodel outputs
    
    * fix bug and prevent spawner.recruit.curves.R script from viewing outputs.long (habmodel outputs in long form)
    
    * Changed file path from Outputs_dir to outputs_hab
    
    * changed "productivity" to "survival" to match format of other scripts
    
    * Add asrp scenarios to long form habmodel outputs
    
    * create single output for each species that contains all habitat scenarios
    
    * Change "old_file_folder" to dev branch outputs, refactoring to fit new data input
    
    * Create comparison tool for habitat model.
    
    * moved calculation of habmodel comparisons out of data frame and into write.csv function
    
    Approved-by: Colin Nicol - NOAA Affiliate <colin.nicol@noaa.gov>
    Approved-by: Caleb Fogel - NOAA Affiliate <caleb.fogel@noaa.gov>

--- hab/R_files/Run_Habitat_Model.R
+++ hab/R_files/Run_Habitat_Model.R
@@ -16,7 +16,6 @@ if (dir.exists(outputs_hab) == F) {
   dir.create(outputs_hab, recursive = TRUE)
 }
 
-  
 source("hab/R_files/inputs.R")
 source("hab/R_files/functions.R")
 source("hab/R_files/flowline.R")
@@ -30,7 +29,6 @@ source("hab/R_files/prespawn.R")
 source("hab/R_files/EggToFry_Survival.R")
 source("hab/R_files/compute_habmodel_outputs.R")
 source("hab/R_files/asrp_files/asrp_scenarios.R")
-# if (!branch %in% c("dev", "master")) {
-  # source("hab/R_files/compare_scenarios.R")
-# }
+source("hab/R_files/compare_scenarios.R")
+
 # save.image(file ='R files/myEnvironment.RData')
