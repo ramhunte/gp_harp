3dedd04901935f5450513af462098b981bdaea10
Colin Nicol <colin.nicol@noaa.gov>
2019-09-17 15:35:05 -0700

    Script and up and running to replace each basin in the Current scenario one by one with conditions from a different scenario (e.g. Floodplain, Shade).

--- lcm/scripts/historical_ms_only.R
+++ lcm/scripts/historical_ms_only.R
@@ -5,11 +5,30 @@
 # will rerun, but this time each scenario will have historical conditions in the mainstem only
 # E.g. Floodplain will now be current conditions in all basins except historical FP in the mainstem
 
-path_to_hab <- file.path('outputs', fishtype, 'hab.scenarios')
+# WARNING: This will delete all habitat files currently in the directory
 
-# Get Current conditions for basins 1:51
+
+# Parameters to set ----
+
+resto_scenario <- 'Shade'
+
+
+############################## Should run below here ########################################
+
+
+# Load packages ----
+
+library(sf)
+
+# Setup params ----
+path_to_hab <- file.path('outputs', fishtype, 'hab.scenarios')
+subs <- paste0('X', 1:63) %>% as.list
+
+
+# functions ----
 
 hab_to_long <- function(scenario_name) {
+  # Make hab scenario long form, so it is easier to manipulate
   scenario_name %>%
     list.files(path_to_hab, pattern = ., full.names = TRUE) %>%
     read.csv %>%
@@ -17,66 +36,52 @@ hab_to_long <- function(scenario_name) {
     gather(subbasin, value, X1:X63)
 }
 
-# For now, don't do this on the ASRP scenarios
-curr <- 'Current.csv' %>%
-  hab_to_long()
-
-fp <- 'Floodplain.csv' %>%
-  hab_to_long()
-
-subs <- list(paste0('X', 1:51)) %>% append(., paste0('X', 52:63))
-names(subs) <- c('X0', paste0('X', 52:63))
-# Swap in Current conditions for basins 1:51, leave 52:63 as is
-
-# Replace one ms basin with historical conditions
-
 replace_ms_sub <- function(sub_to_replace) {
-  curr %>%
+  # Replace a Current subbasin with the column from resto_scenario
+  'Current.csv' %>%
+    hab_to_long() %>%
     filter(!subbasin %in% sub_to_replace) %>%
-    bind_rows(fp %>%
+    bind_rows(resto_scenario %>%
+                paste0(., '.csv') %>%
+                hab_to_long() %>%
                 filter(subbasin %in% sub_to_replace)) %>%
     mutate(subbasin = as.numeric(str_remove(subbasin, 'X'))) %>%
     spread(subbasin, value) %>%
     write.csv(file.path(path_to_hab,
-                        paste0('Floodplain_', sub_to_replace[1], '.csv')))
+                        paste0(resto_scenario,'_', sub_to_replace, '.csv')))
 }
 
-
+# Create hab scenarios with restoration in one basin ---- 
 lapply(subs , replace_ms_sub)
-  
- 
-# # Store the original model run
-# 
-# orig <- read.csv('outputs/coho/lcm/coho_abundance_by_subbasin.csv') #%>%
-# #select(natal.basin, scenario, spawners) %>%
-# # rename(spawners_orig = spawners)
-# 
-# source('lcm/LCM.sim.R')
-# 
-# y <- orig %>%
-#   select(natal.basin, scenario, spawners) %>%
-#   spread(scenario, spawners) %>%
-#   mutate(FP_diff_orig = Floodplain - Current) %>%
-#   select(natal.basin, FP_diff_orig)
-# 
-# 
 
+# Run the LCM ----
+source('lcm/LCM.sim.R')
 
-read.csv('outputs/coho/lcm/coho_abundance_by_subbasin.csv') %>%
-  filter(scenario %in% c('Current', 'Floodplain.X1')) %>%
+# Summarize
+# Difference between current and ScenarioY.X12 is the differnece made by doing ScenarioY in basin 12
+df_diff <- read.csv('outputs/coho/lcm/coho_abundance_by_subbasin_raw.csv') %>%
+  select(natal.basin, scenario, spawners) %>%
+  group_by(natal.basin) %>%
+  mutate(curr = spawners[scenario == 'Current'],
+         diff = spawners - curr,
+         diff_prcnt = diff/curr, 
+         hist_basin = str_extract(scenario, "[^X]+$") %>% as.integer) %>%
   left_join(subbasins %>% rename(natal.basin = Subbasin)) %>%
-  select(scenario, natal.basin, Subbasin_num, spawners) %>%
-  spread(scenario, spawners) %>%
-  mutate(diff = Floodplain.X1 - Current) %>%
-  filter(!Subbasin_num %in% 52:63) %>%
-  bind_rows(read.csv('outputs/coho/lcm/coho_abundance_basinwide.csv') %>%
-              filter(str_detect(scenario, 'Current|Floodplain')) %>%
-              mutate(curr = basinwide_spawners[scenario == 'Current'],
-                     diff = basinwide_spawners - curr) %>%
-              filter(str_detect(scenario, 'X5|X6')) %>%
-              mutate(Subbasin_num = str_extract(scenario, "[^X]+$") %>% as.integer) %>%
-              select(Subbasin_num, diff)) %>%
-  select(Subbasin_num, diff)
+  filter(hist_basin == Subbasin_num) %>%
+  rename(noaa_sub_num = Subbasin_num) %>%
+  select(noaa_sub_num, scenario, spawners, curr, diff, diff_prcnt)
+
+
+# Delete extra files
+list.files(path_to_hab, pattern = ".X", full.names = TRUE) %>% unlink
+
+# # Create shapefile
+# gdb_in <- '//nwcfile/FE/watershed/Chehalis/Data/10-Habitat data layers/SpatialModel_Archive/20190604/Inputs.gdb'
+# 
+# st_read(dsn = gdb_in, layer = 'NOAA_subbasins_w_fp') %>%
+#   left_join(df_diff) %>%
+#   st_write('../maps/Floodplain_resto_benefit.shp')
+
   
 
 
