a627ba5df59752893d11e671ade5f716a6389be2
Colin Nicol <colin.nicol@noaa.gov>
2020-01-17 15:58:09 -0800

    Peak flow analysis now running all the way through
    * Change run_agu.R to run_reak_flow.R
    * Add the fit of the LPIII distribution to the skagit and chehalis data
    * Convert RI to scaled survivals
    * Run model with new survivals

---
+++ run_peak_flow.R
@@ -0,0 +1,174 @@
+
+
+# Script to run the model to produce figures for AGU 2019
+
+library(tidyverse)
+library(lubridate)
+
+rmarkdown::render('docs/future_flows.Rmd')
+
+pops <-  c('coho', 'spring_chinook', 'fall_chinook', 'steelhead')
+run_stochastic_eggtofry <- 'yes'
+sensitivity.mode <- 'no'
+
+
+# Number of years the model will run for
+years <- length(peak_ch$q_cfs)
+runs <- 5
+reach.names <- read.csv("lcm/data/subbasin_names.csv") %>%
+  select(Subbasin) %>%
+  mutate(Subbasin = as.character(Subbasin)) %>%
+  unlist(use.names = FALSE)
+
+model.all.pf <- array(
+  NA,
+  c(runs,
+    years,
+    1,
+    length(pops)
+  ),
+  dimnames = list(
+    1:runs,
+    1:years, 
+    'spawners', 
+    pops
+  )
+)
+
+
+incubation_months <- list(
+  c(11,12,1:4), # coho
+  c(9:12, 1:3), # spring chinook
+  c(10:12, 1:4), # fall chinook
+  c(3:9) # steelhead
+)
+
+for (pop in pops) {
+  
+  surv_df <- daily_ch %>%
+    mutate(month = month(Date)) %>%
+    filter(month %in% incubation_months[[which(pop == pops)]]) %>%
+    group_by(waterYear) %>%
+    summarize(Q_max = max(Q)) %>%
+    mutate(ri = q_to_ri(Q_max, Q_max, 0.14)) %>% # chehalis = 0.14 skewness
+    mutate(diff_perc_rcp45_2050 = predict(mods$fit[[1]], newdata = .),
+           diff_perc_rcp45_2080 = predict(mods$fit[[2]], newdata = .),
+           diff_perc_rcp85_2050 = predict(mods$fit[[3]], newdata = .),
+           diff_perc_rcp85_2080 = predict(mods$fit[[4]], newdata = .),
+           Q_rcp45_2050 = Q_max + Q_max * diff_perc_rcp45_2050,
+           Q_rcp45_2080 = Q_max + Q_max * diff_perc_rcp45_2080,
+           Q_rcp85_2050 = Q_max + Q_max * diff_perc_rcp85_2050,
+           Q_rcp85_2080 = Q_max + Q_max * diff_perc_rcp85_2080) %>%
+    mutate(surv_cur = ri_to_surv_rescale(ri),
+           surv_rcp45_2050 = q_to_ri(Q_rcp45_2050, Q_max, 0.14) %>% ri_to_surv_rescale(),
+           surv_rcp45_2080 = q_to_ri(Q_rcp45_2080, Q_max, 0.14) %>% ri_to_surv_rescale(),
+           surv_rcp85_2050 = q_to_ri(Q_rcp85_2050, Q_max, 0.14) %>% ri_to_surv_rescale(),
+           surv_rcp85_2080 = q_to_ri(Q_rcp85_2080, Q_max, 0.14) %>% ri_to_surv_rescale())
+  
+  
+  surv_gm <- list(
+    surv_df %>%
+      pull(surv_cur)
+    ,
+    surv_df %>%
+      pull(surv_rcp45_2050)
+    ,
+    surv_df %>%
+      pull(surv_rcp45_2080)
+    ,
+    surv_df %>%
+      pull(surv_rcp85_2050)
+    ,
+    surv_df %>%
+      pull(surv_rcp85_2080)
+  )
+  
+  source(list.files('lcm/params', pattern = str_extract(pop, "[^_]+"), full.names = TRUE))
+  source('lcm/scripts/funcs.R')
+  source('lcm/scripts/initialize.R')
+  source("lcm/scripts/assign.dat.R")
+  
+  spawner.init <- list.files(file.path('outputs',pop), 
+                             pattern = 'abundance_by_subbasin.csv', 
+                             recursive = TRUE,
+                             full.names = TRUE) %>%
+    read.csv %>%
+    filter(scenario == 'Current') %>%
+    pull(spawners)
+  
+  # Run model 7 generations for each scenario ----
+  
+  for (r in 1:runs) { # runs loop
+    
+    ef_flows <- surv_gm[[r]]
+    
+    ef_flow <- 1
+    # initialize
+    for (y in 1:10) {
+      N.initialize['spawners', ] <- spawner.init * 0.75
+      N.initialize <- subbasin(mat = N.initialize) 
+    }
+    
+    N <- N.initialize
+    
+    
+    for (y in 1:years) {
+      
+      ef_flow <- ef_flows[y]
+      
+      N <- subbasin(mat = N)
+      
+      model.all.pf[r, y, "spawners", pop] <- sum(N['spawners',])
+    }
+  }
+}
+
+
+
+x <- model.all.pf[,,'spawners',] %>%
+  as.data.frame.table() %>%
+  rename(run = Var1, year = Var2, species = Var3, n = Freq) %>%
+  mutate(year = as.numeric(year),
+         era = case_when(
+           run == 1 ~ 'Current',
+           run %in% c(2,4) ~ 'Mid-century',
+           run %in% c(3,5) ~ 'Late-century'),
+         climate = case_when(
+           run == 1 ~ 'Current',
+           run %in% c(2,3) ~ 'RCP 4.5',
+           run %in% c(4,5) ~ 'RCP 8.5'),
+         species = recode_factor(species,
+                                 coho = 'Coho',
+                                 spring_chinook = 'Spring Chinook',
+                                 fall_chinook = 'Fall Chinook',
+                                 steelhead = 'Steelhead')) %>%
+  group_by(species) %>%
+  mutate(perc_diff = (n - median(n[era == 'Current']))/median(n[era == 'Current']))
+
+        
+
+print(
+  x %>%
+    ggplot(aes(era,perc_diff, color = climate)) +
+    geom_boxplot(outlier.shape = NA) +
+    geom_point(position = position_jitterdodge(jitter.width = 0.1), alpha = 0.3) +
+    facet_wrap(~species) +
+    theme_bw() +
+    scale_color_manual(values = c('black','orange1','orangered2')) +
+    scale_y_continuous(labels = scales::percent) +
+    labs(x = NULL, y = 'Spawner Change from Current (%)', color = NULL)
+)
+
+print(
+  ggplot(x) +
+    geom_line(aes(year, n, color = era, lty = climate)) +
+    facet_wrap(~species, ncol = 1, scales = 'free_y')
+)
+
+x %>%
+  group_by(species, era, climate) %>%
+  summarize(min = min(perc_diff),
+            median = median(perc_diff),
+            max = max(perc_diff))
+
+
