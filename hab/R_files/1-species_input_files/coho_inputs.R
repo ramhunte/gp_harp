ece52844a3e43d6f32752db3a7009c84b0019697
Colin Nicol - NOAA Affiliate <colin.nicol@noaa.gov>
2020-01-27 23:41:52 +0000

    Merged in feature-update_coho_density (pull request #203)
    Update coho densities
    
    Approved-by: Colin Nicol - NOAA Affiliate <colin.nicol@noaa.gov>

--- hab/R_files/1-species_input_files/coho_inputs.R
+++ hab/R_files/1-species_input_files/coho_inputs.R
@@ -10,7 +10,7 @@ NF_redd_density = 12
 Habitat = c(rep(c("Bank", "HM_Bank", "Bar_boulder", "Bar_gravel", "Bar_sand", "Backwater", "Pool", "Riffle", "Beaver.Pond", "FP_Channel", "Lake", "Marsh", "FP_Pond_lg", "FP_Pond_sm", 
             "SC_pool", "SC_riffle", "Slough_lg", "Slough_sm"), times = 2))
 life.stage = c(rep("summer", times = length(Habitat)/2), rep("winter", times = length(Habitat)/2))
-Density = c(5400, 1996, 0, 1000, 1000, 7900, 17000, 3000, 12000, 0, 0, 0, 9000, 18000, 17000, 3000, 9000, 18000,540, 500, 0, 0, 0, 600, 4000, 100, 12000, 0,25,3200, 9000, 18000, 
+Density = c(19600, 9600, 0, 0, 0, 18600, 17000, 3000, 12000, 0, 0, 0, 9000, 18000, 17000, 3000, 9000, 18000,3200, 0, 0, 0, 0, 6400, 4000, 100, 12000, 0,25,3200, 9000, 18000, 
             4000, 100, 9000, 18000)              
 density <- data.frame(Habitat,life.stage, Density)
 
