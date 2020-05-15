e6479df1cf5d85bb4f6f2c1193ffe960ab0c2d6c
Caleb Fogel - NOAA Affiliate <caleb.fogel@noaa.gov>
2020-05-15 22:25:53 +0000

    Merged in feature-remove_psu_temps (pull request #218)
    Feature remove psu temps
    
    * Remove use of PSU temperaturesfrom model
    
    * Outputs
    
    * Outputs
    
    * Refactoring
    
    * Delete input psu temps file
    
    Approved-by: Colin Nicol - NOAA Affiliate <colin.nicol@noaa.gov>
    Approved-by: Caleb Fogel - NOAA Affiliate <caleb.fogel@noaa.gov>

--- hab/R_files/3-temperature.R
+++ hab/R_files/3-temperature.R
@@ -15,47 +15,6 @@ if (fishtype %in% c('spring_chinook', 'fall_chinook')) {
 }
 
 
-# PSU temperatures ----
-
-# Calculate mean weekly maximum temperature to be used in rearing life stages ----
-
-psu_temps_mwmt <- read.csv("hab/Inputs/temperature_inputs/PSU_Modeled_Temperatures_current.csv") %>%
-  separate(col = JDAY, c("month", "day"), sep = "/") %>%
-  gather(reach_pt, Temperature, X1:X322) %>%
-  mutate(species = fishtype) %>%
-  filter(month == 8,
-         Temperature > 0) %>%
-  group_by(year,reach_pt) %>%
-  mutate(mwmt = zoo::rollmean(Temperature, 7, na.pad = TRUE, align = "right")) %>%
-  group_by(year,reach_pt) %>%
-  summarize(mwmt = max(mwmt, na.rm = T)) %>%
-  group_by(reach_pt) %>%
-  summarize(mwmt = max(mwmt, na.rm = T)) %>%
-  mutate(prespawn_temp_psu = mwmt)
-
-# June 1-21 mean daily maximum used for chinook downstream migration rearing
-psu_temps_mean_daily_max_june <- read.csv("hab/Inputs/temperature_inputs/PSU_Modeled_Temperatures_current.csv") %>%
-  separate(col = JDAY, c("month", "day"), sep = "/") %>%
-  gather(reach_pt, Temperature, X1:X322) %>%
-  filter(month == 6,
-         day %in% 1:21,
-         Temperature > 0) %>%
-  group_by(reach_pt) %>%
-  summarize(mdm_june = mean(Temperature, na.rm = T))
-
-
-psu_temps <- psu_temps_mwmt %>%
-  left_join(., psu_temps_mean_daily_max_june, by = "reach_pt") %>%
-  mutate(Seg = as.numeric(sub('.', '', reach_pt))) %>%
-  select(-reach_pt) %>%
-  mutate(
-    rearing_temp_psu = case_when(
-      fishtype %in% c('spring_chinook', 'fall_chinook') ~ mdm_june,
-      fishtype %in% c('coho', 'steelhead') ~ mwmt),
-    prespawn_temp_psu  = mwmt)
-
-# write.csv(psu_lyr, 'hab/Inputs/psu_temps.csv')
-
 # Create temperature data frame and fill data gaps ----
 
 # Create single temperature dataframe by combining thermalscape_temps and psu_temps dataframes.  Where psu temperature data exist, we use the psu temperatures.
@@ -65,20 +24,9 @@ psu_temps <- psu_temps_mwmt %>%
 all_temps <- flowline %>%
   select(noaaid, Reach, Seg, Habitat) %>%
   left_join(., thermalscape_temps, by = 'Reach') %>%
-  left_join(., psu_temps, by = 'Seg') %>%
   mutate(
-    rearing_temp_psu = ifelse(Habitat == 'LgRiver',
-                              rearing_temp_psu,
-                              NA),
-    prespawn_temp_psu = ifelse(Habitat == 'LgRiver',
-                               prespawn_temp_psu,
-                               NA),
-    rear_temp = case_when(
-      !is.na(rearing_temp_psu) ~ rearing_temp_psu,
-      is.na(rearing_temp_psu) & !is.na(rearing_temp_thermal) ~ rearing_temp_thermal),
-    prespawn_temp = case_when(
-      !is.na(prespawn_temp_psu) ~ prespawn_temp_psu,
-      is.na(prespawn_temp_psu) & !is.na(prespawn_temp_thermal) ~ prespawn_temp_thermal)
+    rear_temp = rearing_temp_thermal,
+    prespawn_temp = prespawn_temp_thermal
     ) %>%
   gather(type, temp, c(rear_temp, prespawn_temp)) %>%
   spread(type, temp) %>%
