160f0e72c4f86232374a45f07f80a1f68c9d0647
Caleb.Fogel <caleb.fogel@noaa.gov>
2020-05-18 10:44:52 -0700

    Add in current spawners to diagnostic scenarios tab, remove current and historical scenarios from scenario column

--- srt_figures/spawners_diagnostic_scenarios.R
+++ srt_figures/spawners_diagnostic_scenarios.R
@@ -1,5 +1,5 @@
 diagnostic_species <- c('coho', 'spring_chinook', 'fall_chinook', 'steelhead')
-spawners_diagnostics <- lcm_results <- lapply(lcm_species, function(l) {
+spawners_diagnostics <- lcm_results <- lapply(diagnostic_species, function(l) {
   
   read.csv(paste0('outputs/', l, '/lcm/', str_replace(l, '_', '.'), '_abundance_by_subbasin.csv')) %>%
     mutate(species = l) %>%
@@ -7,29 +7,30 @@ spawners_diagnostics <- lcm_results <- lapply(lcm_species, function(l) {
     select(Subbasin, scenario,spawners, species) %>%
     filter(scenario %in% diag_scenarios) %>%
     group_by(Subbasin, species) %>%
-    mutate(Current = spawners[scenario == 'Current'],
-           restoration_potential = spawners - Current) 
+    mutate(Current_spawners = spawners[scenario == 'Current'],
+           restoration_potential = spawners - Current_spawners) 
 }) %>%
   do.call('rbind',.) %>%
   group_by(species) %>%
   mutate(spawners_basinwide = sum(spawners[scenario == 'Current'])) %>%
   ungroup() %>%
   # group_by(scenario) %>%
-  mutate(perc_of_basin = Current / spawners_basinwide,
+  mutate(perc_of_basin = Current_spawners / spawners_basinwide,
          rank_diff = rank(-restoration_potential, ties.method = 'min'),
-         rank_curr = rank(-Current, ties.method = 'min'),
+         rank_curr = rank(-Current_spawners, ties.method = 'min'),
          rank_weighted = rank(-perc_of_basin, ties.method = 'min')) %>%
   
   rowwise() %>%
   mutate(rank = mean(c(rank_diff, rank_curr)),
-         rank_wt = mean(c(rank_diff, rank_weighted))) %>%
-  select(species, Subbasin, scenario, spawners, restoration_potential, rank, rank_wt) %>%
+         rank_wt = mean(c(rank_diff, rank_weighted))) %>% 
+  filter(!scenario %in% c('Current', 'Historical')) %>%
+  select(species, Subbasin, scenario, Current_spawners, spawners, restoration_potential, rank, rank_wt) %>%
   # gather(group, data, c(restoration_potential, spawners, rank, rank_wt)) %>%
   # unite(scenario, group, col = 'scenario_data') %>%
   # spread(scenario_data, data) %>%
   left_join(., subbasin_names) 
 
-spawners_diagnostics <- spawners_diagnostics[c('species', 'Subbasin', 'Subbasin_num', 'EcoRegion', 'scenario','spawners', 'restoration_potential', 'rank', 'rank_wt')]
+spawners_diagnostics <- spawners_diagnostics[c('species', 'Subbasin', 'Subbasin_num', 'EcoRegion', 'scenario','Current_spawners', 'spawners', 'restoration_potential', 'rank', 'rank_wt')]
 # spawners_diagnostics_2 <- spawners_diagnostics[c('species', 'Subbasin', 'Subbasin_num', 'EcoRegion', 'Current_spawners',
                                                # 'Barriers_spawners', 'Barriers_restoration_potential', 'Barriers_rank', 'Barriers_rank_wt',
                                                # 'Beaver_spawners', 'Beaver_restoration_potential', 'Beaver_rank', 'Beaver_rank_wt',
