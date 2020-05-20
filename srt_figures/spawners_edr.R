27aa4027018e4f92ac63b711144238ed018b7d18
Caleb.Fogel <caleb.fogel@noaa.gov>
2020-05-20 10:59:51 -0700

    updates

--- srt_figures/spawners_edr.R
+++ srt_figures/spawners_edr.R
@@ -1,11 +1,11 @@
-
+#### Current and historical only ----
 edr_species <- c('coho', 'spring_chinook', 'fall_chinook', 'steelhead')
 
 spawners_edr <- lapply(edr_species, function(s) {
   read.csv(paste0('outputs/', s, '/lcm/', str_replace(s, '_', '.'), '_abundance_by_EDR.csv')) %>%
-    mutate(species = s) %>%
+    mutate(species = capitalize(str_replace(s, '_', ' '))) %>%
     select(species,scenario, EcoRegion, basinwide_spawners) 
-    }) %>%
+}) %>%
   do.call('rbind',.) %>%
   filter(scenario %in% c('Historical', 'Current')) %>%
   spread(scenario, basinwide_spawners) %>%
@@ -13,33 +13,50 @@ spawners_edr <- lapply(edr_species, function(s) {
   mutate(spawners_basinwide = sum(Current, na.rm = T)) %>%
   ungroup() %>%
   left_join(flowline_edr) %>%
-  mutate(restoration_potential = Historical - Current,
-         spawners_per_km = Current / length_km,
-         perc_of_basin = Current / spawners_basinwide,
-         rank_diff = rank(-restoration_potential, ties.method = 'min'),
-         # rank_curr = rank(-Current, ties.method = 'min'),
-         rank_weighted = rank(-perc_of_basin, ties.method = 'min'),
-         rank_weighted_spawner_per_km = rank(-spawners_per_km, ties.method = 'min')) %>%
-  rowwise() %>%
-  mutate(
-    # rank_mean = mean(c(rank_diff, rank_curr)),
-         rank_wt_mean = mean(c(rank_diff, rank_weighted)),
-         rank_wt_per_km_mean = mean(c(rank_diff, rank_weighted_spawner_per_km)),
-         ) %>%
-  ungroup() %>%
-  mutate(
-    # rank = rank(rank_mean, ties.method = ''),
-         rank_wt = rank(rank_wt_mean, ties.method = 'min'),
-         rank_wt_per_km = rank(rank_wt_per_km_mean, ties.method = 'min')) %>%
+  mutate(length_km = round(length_km, digits = 2),
+         restoration_potential = Historical - Current,
+         spawners_per_km = round(restoration_potential / length_km, digits = 1),
+         perc_of_pop = round(restoration_potential / spawners_basinwide * 100, digits = 2)) %>%
   group_by(species) %>%
-  mutate(rank_species = rank(rank_diff, ties.method = 'min'),
-         rank_wt_km_species = rank(rank_wt_per_km, ties.method = 'min')) %>%
-select(species, EcoRegion, lenght_km, Current, Historical, restoration_potential,rank_diff, rank_species, spawners_per_km,rank_wt_per_km, rank_wt_km_species)
-  
+  mutate(rank_rest_potential = rank(-restoration_potential, ties.method = 'min'),
+         rank_spawner_per_km = rank(-spawners_per_km, ties.method = 'min')) %>%
+  ungroup() %>%
+  mutate(rank_percent = rank(-perc_of_pop, ties.method = 'min')) %>%
+  select(species, EcoRegion, length_km, Current, Historical, restoration_potential, rank_rest_potential, spawners_per_km, rank_spawner_per_km, perc_of_pop, rank_percent) 
 
-colnames(spawners_edr) <- c('Species', 'EcoRegion', 'Total Km', 'Current spawners', 'Historical spawners', 'Restoration potential (spawners)', 'Rank (spawners)',
-                            'Rank (spawners by species)', 'Spawners per km', 'Rank (spawners/km)', 'Rank (spawners/km by species)')
-
+colnames(spawners_edr) <- c('Species', 'EcoRegion', 'Total Km', 'Current spawners', 'Historical spawners', 'Restoration potential (spawners)', 
+                            'Rank (spawners)', 'Spawners per km', 'Rank (spawners/km)', 'Percent change', 'Rank (percent change)')
 
 write.csv(spawners_edr, 'srt_figures/results/spawners_edr.csv')
 
+#### Diagnostic scenarios included ----
+spawners_edr_diagnostics <- lapply(edr_species, function(t) {
+  read.csv(paste0('outputs/', t, '/lcm/', str_replace(t, '_', '.'), '_abundance_by_EDR.csv')) %>%
+    mutate(species = capitalize(str_replace(t, '_', ' '))) %>%
+    select(species,scenario, EcoRegion, basinwide_spawners) %>%
+    filter(scenario %in% diag_scenarios) %>%
+    group_by(EcoRegion, species) %>%
+    mutate(Current_spawners = basinwide_spawners[scenario == 'Current'],
+           restoration_potential = basinwide_spawners - Current_spawners)
+}) %>%
+  do.call('rbind',.) %>%
+  rename(spawners = basinwide_spawners) %>%
+  group_by(species) %>%
+  mutate(spawners_basinwide = sum(spawners[scenario == 'Current'])) %>%
+  filter(!scenario %in% c('Current', 'Historical')) %>%
+  ungroup() %>%
+  left_join(., flowline_edr) %>%
+  mutate(length_km = round(length_km, digits = 2),
+         perc_of_pop = round(restoration_potential / spawners_basinwide * 100, digits = 2),
+         spawners_per_km = round(Current_spawners / length_km, digits = 1)) %>%
+  group_by(species) %>%
+  mutate(rank_rest_potential = rank(-restoration_potential, ties.method = 'min'),
+         rank_spawner_per_km = rank(-spawners_per_km, ties.method = 'min')) %>%
+  ungroup() %>%
+  mutate(rank_percent = rank(-perc_of_pop, ties.method = 'min')) %>%
+  select(species, EcoRegion, length_km, scenario, Current_spawners, spawners, restoration_potential, rank_rest_potential, spawners_per_km, rank_spawner_per_km, perc_of_pop, rank_percent)
+
+colnames(spawners_edr_diagnostics) <- c('Species', 'EcoRegion', 'Total Km', 'Diagnostic scenario', 'Current spawners', 'Diagnostic scenario spawners', 'Restoration potential (spawners)', 
+                                        'Rank (spawners)', 'Spawners per km', 'Rank (spawners/km)', 'Percent change', 'Rank (percent change)')
+
+write.csv(spawners_edr_diagnostics, 'srt_figures/results/spawners_edr_diagnostics.csv')
