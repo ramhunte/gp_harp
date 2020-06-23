690e50eeda3225a182077e3e3e49d5ed7d460b72
Caleb Fogel - NOAA Affiliate <caleb.fogel@noaa.gov>
2020-06-23 22:04:02 +0000

    Merged in feature-chinook_width_may (pull request #221)
    Use may widths for chinook
    
    * Use may widths for chinook
    
    * Outputs
    
    * Outputs part 2
    
    Approved-by: Colin Nicol - NOAA Affiliate <colin.nicol@noaa.gov>
    Approved-by: Caleb Fogel - NOAA Affiliate <caleb.fogel@noaa.gov>

--- hab/R_files/2-read_in_data.R
+++ hab/R_files/2-read_in_data.R
@@ -10,9 +10,19 @@ subbasin_names <- read.csv("hab/Inputs/Subbasin_names.csv")
 # EDT summer and winter wetted widths.  Joined to flowline using "Reach".  Contains current, historical, 2040 and 2080 widths ----
 edt_width <- list.files(path = Inputs, pattern = "edt_width.csv", full.names = T) %>%
   read.csv(.) %>%
-  mutate(Reach_low = tolower(Reach)) %>%
-  rename(width_w = X1,
-         width_s = X8) %>%
+  mutate(Reach_low = tolower(Reach))
+
+if (fishtype %in% c('spring_chinook', 'fall_chinook')) {
+  edt_width %<>%
+    rename(width_w = X1,
+         width_s = X5)
+} else {
+  edt_width %<>%
+    rename(width_w = X1,
+           width_s = X8)
+}
+
+  edt_width %<>%
   select(Reach_low, width_w, width_s, year) %>%
   group_by(Reach_low) %>%
   mutate(width_s_curr = ifelse(year == 2019,
