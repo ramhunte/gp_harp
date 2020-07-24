a7d8f1c869e470704f1732962ec8cb5cf937d688
Colin Nicol - NOAA Affiliate <colin.nicol@noaa.gov>
2020-07-24 17:21:45 +0000

    Merged in feature-chum_recalibration (pull request #227)
    Feature chum recalibration
    
    * Redo batch of outputs. Fixes coho ASRP hab scenarios and steelhead LCM results.
    
    * batch of output files
    
    * Changed weekly p to 0.8 and the rearing capacity multiplier to 1.1.
    
    * Fix merge conflicts: merge with dev
    
    Merge branch 'dev' into feature-chum_recalibration
    
    # Conflicts:
    #	outputs/steelhead/lcm/steelhead_abundance_by_subbasin.csv
    #	outputs/steelhead/lcm/steelhead_abundance_by_subbasin_raw.csv
    
    * Update parameters after small group meeting on 6/24/2020
    
    * Fix merge conflicts
    
    Merge remote-tracking branch 'origin/dev' into feature-chum_recalibration
    
    # Conflicts:
    #	hab/R_files/2-read_in_data.R
    
    * Tweaks to chum params.
    
    * Adjust p_weekly to 0.8 and reduce rearing cap multiplier to 1.
    
    * merge with dev, fix merge conflicts
    
    Merge branch 'dev' into feature-chum_recalibration
    
    # Conflicts:
    #	lcm/scripts/funcs.R
    
    * Recalibrate chum
    
    * Change weekly_p to 0.65, and rearing to weekly_p^2
    * Change colonization_cap to be 8x
    * Print SAR to screen

--- hab/R_files/1-species_input_files/chum_inputs.R
+++ hab/R_files/1-species_input_files/chum_inputs.R
@@ -4,14 +4,14 @@ redd_area = 2.3
 defended_redd_area = 42.3
 adult_per_redd = 1.9
 
-prespawn_surv_raw <- .76
+prespawn_surv_raw <- 1
 
 #Rearing
 Habitat = c(rep(c("Bank", "HM_Bank", "Bar_boulder", "Bar_gravel", "Bar_sand", "Backwater", "Pool", "Riffle", "Beaver.Pond","Pool_low_ag", 
                   "FP_Channel", "Lake", "Marsh", "FP_Pond_lg", "FP_Pond_sm", "SC_pool", "SC_riffle", "Slough_lg", "Slough_sm"), times = 4))
-life.stage = c(rep("fry.colonization", times = length(Habitat)/4), rep("winter", times = length(Habitat)/4), rep('summer.2', times = length(Habitat) / 4), 
+life.stage = c(rep("summer", times = length(Habitat)/4), rep("fry.colonization", times = length(Habitat)/4), rep('summer.2', times = length(Habitat) / 4), 
                rep('winter.2', times = length(Habitat) / 4))
-Density = c(rep(13300, times = 2), rep(17000, times = 2), 6700, 30000, rep(30000, times = 2), 0, 30000,rep(0, times = 66))
+Density = c(rep(0, times = 19), rep(13300, times = 2), rep(17000, times = 2), 6700, 30000, rep(30000, times = 2), 0, 30000,rep(0, times = 47))
 density <- data.frame(Habitat, life.stage, Density)
 
 winter_pool_scalar_warm = 1
@@ -19,12 +19,14 @@ winter_pool_scalar_cold = 1
 
 #Survival#
 
+p_weekly <- 0.8
+
 Habitat = c(rep(c("Bank", "HM_Bank", "Bar_boulder", "Bar_gravel", "Bar_sand", "Backwater", "Pool", "Riffle", "Beaver.Pond", "FP_Channel", "Lake", "Marsh", "FP_Pond_lg", "FP_Pond_sm", 
                   "SC_pool", "SC_riffle", "Slough_lg", "Slough_sm"), times = 2))
 life.stage = c(rep("summer", times = length(Habitat)/2), rep("winter", times = length(Habitat)/2), rep('fry.colonization', times = length(Habitat) / 2),
                rep('winter.2', times = length(Habitat) / 2))
-surv.base = (c(rep(.44, times = 18), rep(NA, times = 18), rep(.48, times = 18), rep(NA, times = 18)))
-wood.surv.base = (c(rep(.44, times = 18), rep(NA, times = 18), rep(.48, times = 18), rep(NA, times = 18)))
+surv.base = (c(rep(p_weekly^2, times = 18), rep(NA, times = 18), rep(p_weekly, times = 18), rep(NA, times = 18)))
+wood.surv.base = (c(rep(p_weekly^2, times = 18), rep(NA, times = 18), rep(p_weekly, times = 18), rep(NA, times = 18)))
 survival <- data.frame(Habitat, life.stage, surv.base, wood.surv.base)
 
 LgRiver_habs = c("Bank", "HM_Bank", "Bar_boulder", "Bar_gravel", "Bar_sand", "Backwater")
@@ -37,5 +39,5 @@ lr_wd_s_bank <- 1
 lr_wd_s_bar <- 1
 
 # Winter
-lr_wd_w_bank <- -9999
-lr_wd_w_bar <- -9999
+lr_wd_w_bank <- 1
+lr_wd_w_bar <- 1
