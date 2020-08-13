f8dfe4dd7ca187cad726cd790b5456f6eba70599
Caleb.Fogel <caleb.fogel@noaa.gov>
2020-08-13 11:51:20 -0700

    Code to calculate percent change in each population under temperature scenarios

--- temp_paper/calc_percent_chg.R
+++ temp_paper/calc_percent_chg.R
@@ -1,11 +1,12 @@
 View(spawners.paper.all %>% 
-       ungroup() %>% 
-       select(year, scenario.label.nm, prcnt.change, species) %>% 
-       mutate(scenario.label.nm = ifelse(scenario.label.nm == 'Climate Change',
-                                         'cc',
-                                         scenario.label.nm)) %>%
-       spread(scenario.label.nm, prcnt.change) %>%
-       filter(year != 'current') %>%
-       mutate(rip_diff = Riparian - cc,
-              fp_diff = Floodplain - cc,
-              combined_diff = Combined - cc)) 
+             ungroup() %>% 
+             select(year, scenario.label.nm, prcnt.change, species, n) %>% 
+             mutate(scenario.label.nm = ifelse(scenario.label.nm == 'Climate Change',
+                                               'cc',
+                                               scenario.label.nm)) %>%
+             gather(param, value, c(prcnt.change, n)) %>%
+             spread(scenario.label.nm, value) %>%
+             # filter(year != 'current') %>%
+             mutate(rip_diff = Riparian - cc,
+                    fp_diff = Floodplain - cc,
+                    combined_diff = Combined - cc)) 
