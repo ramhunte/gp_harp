a3832d5b598f9592337c5f69a5122063527765df
Caleb.Fogel <caleb.fogel@noaa.gov>
2020-08-13 12:26:26 -0700

    bugfix to calc percent change script

--- temp_paper/calc_percent_chg.R
+++ temp_paper/calc_percent_chg.R
@@ -3,7 +3,7 @@ View(spawners.paper.all %>%
              select(year, scenario.label.nm, prcnt.change, species, n) %>% 
              mutate(scenario.label.nm = ifelse(scenario.label.nm == 'Climate Change',
                                                'cc',
-                                               scenario.label.nm)) %>%
+                                               as.character(scenario.label.nm))) %>%
              gather(param, value, c(prcnt.change, n)) %>%
              spread(scenario.label.nm, value) %>%
              # filter(year != 'current') %>%
